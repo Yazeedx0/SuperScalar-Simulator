ed161e40249f111bff3af3a6a408613df9d8093d
Yazeedx0 <yazed.ka51@gmail.com>
2025-02-22 19:02:37 +0300

    last

--- mips_pipline/InstructionDecoder.py
+++ mips_pipline/InstructionDecoder.py
@@ -1,3 +1,5 @@
+from mips_pipline.enums.ProcessorSignals import Stages, InstructionTypes,RegisterTypes, Instruction
+
 class InstructionDecoder:
     
     @staticmethod
@@ -11,48 +13,53 @@ class InstructionDecoder:
         imm = instruction & 0xFFFF
         address = instruction & 0x3FFFFFF
 
-        if imm & 0x8000:  # تمديد الإشارة
+        if imm & 0x8000:  
             imm = imm - 0x10000
 
         return {
-            'opcode': opcode,
-            'rs': rs,
-            'rt': rt,
-            'rd': rd,
-            'shamt': shamt,
-            'funct': funct,
-            'immediate': imm,
-            'address': address,
-            'type': InstructionDecoder.get_instruction_type(opcode, funct),
-            'mnemonic': InstructionDecoder.get_instruction_name(opcode, funct)
+            RegisterTypes.opcode.value: opcode,
+            RegisterTypes.rs.value: rs,
+            RegisterTypes.rt.value: rt,
+            RegisterTypes.rd.value: rd,
+            RegisterTypes.shamt.value: shamt,
+            RegisterTypes.funct.value: funct,
+            RegisterTypes.immediate.value: imm,
+            RegisterTypes.address.value: address,
+            RegisterTypes.type.value: InstructionDecoder.get_instruction_type(opcode, funct),
+            RegisterTypes.mnemonic.value: InstructionDecoder.get_instruction_name(opcode, funct)
         }
 
     @staticmethod
     def get_instruction_type(opcode: int, funct: int) -> str:
         if opcode == 0:
-            return 'R'
+            return InstructionTypes.R.value
         elif opcode in [0x02, 0x03]:
-            return 'J'
+            return InstructionTypes.J.value
         else:
-            return 'I'
+            return InstructionTypes.I.value
 
     @staticmethod
     def get_instruction_name(opcode: int, funct: int) -> str:
         r_type_map = {
-            0x20: 'ADD', 0x22: 'SUB', 0x24: 'AND', 0x25: 'OR', 0x27: 'NOR',
-            0x26: 'XOR', 0x2A: 'SLT', 0x2B: 'SGT', 0x00: 'SLL', 0x02: 'SRL'
+            0x20: Instruction.ADD.value, 0x22: Instruction.SUB.value, 
+            0x24: Instruction.AND.value, 0x25: Instruction.OR.value, 
+            0x27: Instruction.NOR.value, 0x2A: Instruction.SLT.value, 
+            0x26: Instruction.XOR.value, 0x2B: Instruction.SGT.value, 
+            0x00: Instruction.SLL.value, 0x02: Instruction.SRL.value,
         }
         i_type_map = {
-            0x23: 'LW', 0x2B: 'SW', 0x04: 'BEQ', 0x05: 'BNE', 0x08: 'ADDI',
-            0x0D: 'ORI', 0x0E: 'XORI', 0x01: 'BLTZ_BGEZ'
+            0x23: Instruction.LW.value, 0x2B: Instruction.SW.value,
+            0x0D: Instruction.ORI.value, 0x0E: Instruction.XORI.value,
+            0x01: Instruction.BLTZ_BGEZ.value, 0x04: Instruction.BEQ.value,
+            0x05: Instruction.BNE.value, 0x08: Instruction.ADDI.value,
         }
-        j_type_map = {0x02: 'J', 0x03: 'JAL'}
+        j_type_map = {0x02: Instruction.J.value, 0x03: Instruction.JAL.value}
 
         if opcode == 0:
-            return r_type_map.get(funct, 'UNKNOWN')
+            return r_type_map.get(funct, Instruction.UNKNOWN.value)
         elif opcode in j_type_map:
             return j_type_map[opcode]
         elif opcode == 0x01:
-            return 'BLTZ' if funct == 0 else 'BGEZ'
+            return Instruction.BLTZ.value if funct == 0 else Instruction.BGEZ.value
         else:
-            return i_type_map.get(opcode, 'UNKNOWN')
+            return i_type_map.get(opcode, Instruction.UNKNOWN.value)
